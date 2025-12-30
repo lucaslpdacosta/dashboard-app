a720c7152cd6b0e990d321ecb4fdab2285f21475
lucaslpdacosta <lucaslpdacosta1@gmail.com>
2025-12-29 22:48:41 -0300

    test: return status 404 when get user by id controller doesn't return an user

--- src/controllers/user/get-user-by-id.test.js
+++ src/controllers/user/get-user-by-id.test.js
@@ -40,4 +40,13 @@ describe('GetUserByIdController', () => {
         })
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 404 if a user is not found', async () => {
+        const { sut, getUserByIdUseCase } = makeSut()
+        jest.spyOn(getUserByIdUseCase, 'execute').mockResolvedValue(null)
+        const result = await sut.execute({
+            params: { userId: faker.string.uuid() },
+        })
+        expect(result.statusCode).toBe(404)
+    })
 })
