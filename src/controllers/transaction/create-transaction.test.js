f8a6dffa2275378fafa1b7a04d2151ac062b4178
lucaslpdacosta <lucaslpdacosta1@gmail.com>
2026-01-19 21:47:05 -0300

    test: return status 401 when create transaction controller returns an invalid amount value

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -132,4 +132,15 @@ describe('Create Transaction Controller', () => {
         })
         expect(response.statusCode).toBe(400)
     })
+
+    it('should return 400 when amount is not a valid currency', async () => {
+        const { sut } = makeSut()
+        const response = await sut.execute({
+            body: {
+                ...baseHttpRequest.body,
+                amount: 'invalid_amount',
+            },
+        })
+        expect(response.statusCode).toBe(400)
+    })
 })
