47795f151528d18a91edbcad95cbe948db2142a0
lucas <lucaslpdacosta1@gmail.com>
2026-01-26 23:12:23 -0300

    test: return status 200 when getting a transaction successfully

---
+++ src/controllers/transaction/get-transactions-by-user-id.test.js
@@ -0,0 +1,33 @@
+import { GetTransactionsByUserIdController } from './get-transactions-by-user-id'
+import { faker } from '@faker-js/faker'
+
+describe('Get Transaction By User ID Controller', () => {
+    class GetUserByIdUseCaseStub {
+        async execute() {
+            return [
+                {
+                    user_id: faker.string.uuid(),
+                    id: faker.string.uuid(),
+                    name: faker.commerce.productName(),
+                    date: faker.date.anytime().toISOString(),
+                    type: 'EXPENSE',
+                    amount: Number(faker.finance.amount()),
+                },
+            ]
+        }
+    }
+    const makeSut = () => {
+        const getUserByIdUseCase = new GetUserByIdUseCaseStub()
+        const sut = new GetTransactionsByUserIdController(getUserByIdUseCase)
+
+        return { sut, getUserByIdUseCase }
+    }
+
+    it('should return 200 when finding transaction by user id successfully', async () => {
+        const { sut } = makeSut()
+        const response = await sut.execute({
+            query: { userId: faker.string.uuid() },
+        })
+        expect(response.statusCode).toBe(200)
+    })
+})
