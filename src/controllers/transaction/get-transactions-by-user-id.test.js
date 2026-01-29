e6c262ac91eb9705414d066ab3f466b7e6d22323
lucas <lucaslpdacosta1@gmail.com>
2026-01-28 23:33:23 -0300

    test: return status 400 get transaction controller returs invalid id

--- src/controllers/transaction/get-transactions-by-user-id.test.js
+++ src/controllers/transaction/get-transactions-by-user-id.test.js
@@ -38,4 +38,12 @@ describe('Get Transaction By User ID Controller', () => {
         })
         expect(response.statusCode).toBe(400)
     })
+
+    it('should return 400 when userId param is invalid', async () => {
+        const { sut } = makeSut()
+        const response = await sut.execute({
+            query: { userId: 'invalid_user_id' },
+        })
+        expect(response.statusCode).toBe(400)
+    })
 })
