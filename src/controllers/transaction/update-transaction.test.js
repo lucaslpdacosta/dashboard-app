991b66628ffe0451e94dbd37a10d69b9f67336b9
lucas <lucaslpdacosta1@gmail.com>
2026-02-04 18:43:10 -0300

    test: return status 400 when amount value is invalid

--- src/controllers/transaction/update-transaction.test.js
+++ src/controllers/transaction/update-transaction.test.js
@@ -59,4 +59,16 @@ describe('Update Transaction Controller', () => {
         })
         expect(response.statusCode).toBe(400)
     })
+
+    it('should return 400 when amount is invalid', async () => {
+        const { sut } = makeSut()
+        const response = await sut.execute({
+            ...baseHttpRequest,
+            body: {
+                ...baseHttpRequest.body,
+                amount: 'invalid_amount',
+            },
+        })
+        expect(response.statusCode).toBe(400)
+    })
 })
