ab3f58b30a49cd45fa9cc4e5c84b8fbcb2aba7bc
lucas <lucaslpdacosta1@gmail.com>
2026-02-06 04:51:37 -0300

    test: return status 500 update transaction use case throws an error

--- src/controllers/transaction/update-transaction.test.js
+++ src/controllers/transaction/update-transaction.test.js
@@ -83,4 +83,13 @@ describe('Update Transaction Controller', () => {
         })
         expect(response.statusCode).toBe(400)
     })
+
+    it('should return 500 when UpdateTransactionUseCase throws', async () => {
+        const { sut, updateTransactionUseCase } = makeSut()
+        jest.spyOn(updateTransactionUseCase, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+        const response = await sut.execute(baseHttpRequest)
+        expect(response.statusCode).toBe(500)
+    })
 })
