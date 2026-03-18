c4ffb51cc9483129472c55ab12bd9a03db0fbf5e
lucas <lucaslpdacosta1@gmail.com>
2026-03-17 23:34:16 -0300

    test: return success when user is updates even without passing email or password

---
+++ src/use-cases/user/update-user.test.js
@@ -0,0 +1,60 @@
+import { UpdateUserUseCase } from './update-user'
+import { faker } from '@faker-js/faker'
+
+describe('UpdateUserUseCase', () => {
+    const user = {
+        id: faker.string.uuid(),
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({
+            length: 7,
+        }),
+    }
+
+    class GetUserByEmailRepositoryStub {
+        async execute() {
+            return null
+        }
+    }
+
+    class PasswordHasherAdapterStub {
+        async execute() {
+            return 'hashed_password'
+        }
+    }
+
+    class UpdateUserRepositoryStub {
+        async execute() {
+            return user
+        }
+    }
+
+    const makeSut = () => {
+        const getUserByEmailRepository = new GetUserByEmailRepositoryStub()
+        const updateUserRepository = new UpdateUserRepositoryStub()
+        const passwordHasherAdapter = new PasswordHasherAdapterStub()
+
+        const sut = new UpdateUserUseCase(
+            getUserByEmailRepository,
+            updateUserRepository,
+            passwordHasherAdapter,
+        )
+
+        return {
+            sut,
+            getUserByEmailRepository,
+            updateUserRepository,
+            passwordHasherAdapter,
+        }
+    }
+
+    it('should update user successfully (without email and password)', async () => {
+        const { sut } = makeSut()
+        const result = await sut.execute(faker.string.uuid(), {
+            first_name: faker.person.firstName(),
+            last_name: faker.person.lastName(),
+        })
+        expect(result).toBe(user)
+    })
+})
