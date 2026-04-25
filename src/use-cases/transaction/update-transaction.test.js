25945f90d46a88b5dabd26d431d20274fb734d34
lucas <lucaslpdacosta1@gmail.com>
2026-04-25 00:00:51 -0300

    test: returns success when updating a transaction successfully

---
+++ src/use-cases/transaction/update-transaction.test.js
@@ -0,0 +1,41 @@
+import { faker } from '@faker-js/faker'
+import { UpdateTransactionUseCase } from './update-transaction'
+
+describe('UpdateTransactionUseCase', () => {
+    const transaction = {
+        id: faker.string.uuid(),
+        user_id: faker.string.uuid(),
+        name: faker.commerce.productName(),
+        date: faker.date.anytime().toISOString(),
+        type: 'EXPENSE',
+        amount: Number(faker.finance.amount()),
+    }
+
+    class UpdateTransactionRepositoryStub {
+        async execute(transactionId) {
+            return {
+                id: transactionId,
+                ...transaction,
+            }
+        }
+    }
+
+    const makeSut = () => {
+        const updateTransactionRepository =
+            new UpdateTransactionRepositoryStub()
+        const sut = new UpdateTransactionUseCase(updateTransactionRepository)
+
+        return {
+            sut,
+            updateTransactionRepository,
+        }
+    }
+
+    it('should create a transaction successfully', async () => {
+        const { sut } = makeSut()
+        const result = await sut.execute(transaction.id, {
+            amount: Number(faker.finance.amount()),
+        })
+        expect(result).toEqual(transaction)
+    })
+})
