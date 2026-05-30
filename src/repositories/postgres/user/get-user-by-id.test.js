bdede27bc2a4f8b12b33b1c05eebaf96423adca0
lucas <lucaslpdacosta1@gmail.com>
2026-05-29 23:57:06 -0300

    test: return success when postgres get user by id repository calls prisma with correct params

--- src/repositories/postgres/user/get-user-by-id.test.js
+++ src/repositories/postgres/user/get-user-by-id.test.js
@@ -9,4 +9,15 @@ describe('PostgresGetUserByIdRepository', () => {
         const result = await sut.execute(user.id)
         expect(result).toStrictEqual(user)
     })
+
+    it('should call Prisma with correct params', async () => {
+        const sut = new PostgresGetUserByIdRepository()
+        const prismaSpy = jest.spyOn(prisma.user, 'findUnique')
+        await sut.execute(fakeUser.id)
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                id: fakeUser.id,
+            },
+        })
+    })
 })
