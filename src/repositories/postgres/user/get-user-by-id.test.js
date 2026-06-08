afac2e1b1ef77da9c267972616c5242cf3a98a91
lucas <lucaslpdacosta1@gmail.com>
2026-06-07 23:54:10 -0300

    test: returns an error when prisma throws error on postgres get user by id repository

--- src/repositories/postgres/user/get-user-by-id.test.js
+++ src/repositories/postgres/user/get-user-by-id.test.js
@@ -20,4 +20,11 @@ describe('PostgresGetUserByIdRepository', () => {
             },
         })
     })
+
+    it('should throw if Prisma throws', async () => {
+        const sut = new PostgresGetUserByIdRepository()
+        jest.spyOn(prisma.user, 'findUnique').mockRejectedValueOnce(new Error())
+        const promise = sut.execute(fakeUser.email)
+        await expect(promise).rejects.toThrow()
+    })
 })
