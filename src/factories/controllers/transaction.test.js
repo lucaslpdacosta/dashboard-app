6bec66b32c293b93b654fd7960259719b688de9c
lucas <lucaslpdacosta1@gmail.com>
2026-07-22 23:54:51 -0300

    test: return success when returning a valid DeleteTransactionController instance

--- src/factories/controllers/transaction.test.js
+++ src/factories/controllers/transaction.test.js
@@ -1,9 +1,11 @@
 import {
     CreateTransactionController,
+    DeleteTransactionController,
     UpdateTransactionController,
 } from '../../controllers'
 import {
     makeCreateTransactionController,
+    makeDeleteTransactionController,
     makeUpdateTransactionController,
 } from './transaction'
 
@@ -16,6 +18,11 @@ describe('Transaction Controller Factories', () => {
     it('should return a valid UpdateTransactionController instance', () => {
         expect(makeUpdateTransactionController()).toBeInstanceOf(
             UpdateTransactionController,
+        )
+    })
+    it('should return a valid DeleteTransactionController instance', () => {
+        expect(makeDeleteTransactionController()).toBeInstanceOf(
+            DeleteTransactionController,
         )
     })
 })
