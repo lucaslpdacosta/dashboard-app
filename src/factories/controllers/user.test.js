b346124787642aa9926b622593c71a8c1c65f07c
lucas <lucaslpdacosta1@gmail.com>
2026-08-04 23:40:20 -0300

    test: return success when makeGetUserBalanceController returns valid GetUserBalanceController

--- src/factories/controllers/user.test.js
+++ src/factories/controllers/user.test.js
@@ -1,12 +1,14 @@
 import {
     CreateUserController,
     DeleteUserController,
+    GetUserBalanceController,
     GetUserByIdController,
     UpdateUserController,
 } from '../../controllers'
 import {
     makeCreateUserController,
     makeDeleteUserController,
+    makeGetUserBalanceController,
     makeGetUserByIdController,
     makeUpdateUserController,
 } from './user'
@@ -25,5 +27,10 @@ describe('User Controller Factories', () => {
     })
     it('should return a valid DeleteUserController instance', () => {
         expect(makeDeleteUserController()).toBeInstanceOf(DeleteUserController)
+    })
+    it('should return a valid GetUserBalanceController instance', () => {
+        expect(makeGetUserBalanceController()).toBeInstanceOf(
+            GetUserBalanceController,
+        )
     })
 })
