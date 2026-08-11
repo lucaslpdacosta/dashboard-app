27a58a8b7173c079123b9643e5669349a61e44fd
lucas <lucaslpdacosta1@gmail.com>
2026-08-11 00:02:10 -0300

    feat: added userNotFoundResponse to handle error on create transaction controller

--- src/controllers/transaction/create-transaction.js
+++ src/controllers/transaction/create-transaction.js
@@ -1,21 +1,23 @@
+import { UserNotFoundError } from '../../errors/user.js'
 import { createTransactionSchema } from '../../schemas/transaction.js'
-import { badRequest, created, serverError } from '../helpers/index.js'
+import {
+    badRequest,
+    created,
+    serverError,
+    userNotFoundResponse,
+} from '../helpers/index.js'
 import { ZodError } from 'zod'
 
 export class CreateTransactionController {
     constructor(createTransactionUseCase) {
         this.createTransactionUseCase = createTransactionUseCase
     }
-
     async execute(httpRequest) {
         try {
             const params = httpRequest.body
-
             await createTransactionSchema.parseAsync(params)
-
             const transaction =
                 await this.createTransactionUseCase.execute(params)
-
             return created(transaction)
         } catch (error) {
             if (error instanceof ZodError) {
@@ -23,7 +25,9 @@ export class CreateTransactionController {
                     message: error.errors[0].message,
                 })
             }
-
+            if (error instanceof UserNotFoundError) {
+                return userNotFoundResponse()
+            }
             console.error(error)
             return serverError()
         }
